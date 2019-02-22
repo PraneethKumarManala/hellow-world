6dd1bc99404179df73cdf9424af3cf352a73bc43
PraneethKumarManala <praneethmanala@gmail.com>
2019-02-22 12:29:08 +0530

    .

---
+++ src/test/java/com/testng/jenkins/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.testng.jenkins;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("HelloWorld_Welcome Praneeth");
+	}
+}
---
+++ src/test/java/com/testng/jenkins/HelloWorld1.java
@@ -0,0 +1,13 @@
+package com.testng.jenkins;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld1 {
+
+	@Test
+	public void test1() {
+		
+		System.out.println("HelloWorld1_Welcome Praneeth");
+		
+	}
+}
